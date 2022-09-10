ed1a979c79d405f60b41b4508d031d795461787b
Josimar Renepont <jnrenepont@hotmail.com>
2022-09-10 20:01:50 -0300

    Axios, useEffect first request

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://www.instagram.com/josimarrenepont.ig">@josimarrenepont.ig</a>
                 </p>
             </div>
         </header>
